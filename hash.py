723f17bbdb19bb946d38b773c297a72949d76ec4
Mar-3 <marko.jutila1@gmail.com>
2022-11-27 19:40:39 +0200

    first commit

---
+++ hash.py
@@ -0,0 +1,18 @@
+
+class Hash:
+
+    def __init__(self, slots):
+        self.lsit = []*slots
+
+    def insert():
+        #TODO insetr
+
+    def delete():
+
+    def search():
+
+
+
+
+if __name__ == "__main__":
+    #TODO PÄÄOPHLJEMAL
